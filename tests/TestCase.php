c504dbf65c27c36a7b827dd62e2677c8798e55d6
thannaske <thannaske@users.noreply.github.com>
2022-01-17 22:33:45 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Rescaled\SimpleFeature\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Rescaled\SimpleFeature\SimpleFeatureServiceProvider;
 
